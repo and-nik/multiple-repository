3730a1bc86c7cc71855e87b2bf64582072f3188e
Andrei <iamandry@icloud.com>
2023-04-24 12:54:25 +0300

    init project and add .gitignore

---
+++ MultipleRepository/App/MultipleRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleRepositoryApp.swift
+//  MultipleRepository
+//
+//  Created by And Nik on 20.04.23.
+//
+
+import SwiftUI
+
+@main
+struct MultipleRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RepositoriesView(viewModel: RepositoriesViewModel(networkManager: RepositoriesNetworkManager()))
+        }
+    }
+}
